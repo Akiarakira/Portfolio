70c7961115f742819b7865d7062c8e7cdfdd6513
Andreina Di Brigida <Akiarakirax3@gmail.com>
2024-07-22 22:21:27 -0400

    Add cv, active theme function, func to change logo and styles

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Change theme
+
 const lightMode = document.querySelector("#lightMode");
 const defaultMode = document.querySelector("#defaultMode");
 const darkMode = document.querySelector("#darkMode");
@@ -8,6 +10,10 @@ lightMode.addEventListener("click", function() {
     bodyElement.classList.add("lightMode");
     bodyElement.classList.remove("defaultMode");
     bodyElement.classList.remove("darkMode");
+    lightMode.classList.add("active");
+    darkMode.classList.remove("active");
+    defaultMode.classList.remove("active");
+    changeLogos();
 })
 
 defaultMode.addEventListener("click", function() {
@@ -15,6 +21,10 @@ defaultMode.addEventListener("click", function() {
     bodyElement.classList.add("defaultMode");
     bodyElement.classList.remove("lightMode");
     bodyElement.classList.remove("darkMode");
+    lightMode.classList.remove("active");
+    darkMode.classList.remove("active");
+    defaultMode.classList.add("active");
+    changeLogos();
 })
 
 darkMode.addEventListener("click", function() {
@@ -22,4 +32,23 @@ darkMode.addEventListener("click", function() {
     bodyElement.classList.add("darkMode");
     bodyElement.classList.remove("lightMode");
     bodyElement.classList.remove("defaultMode");
+    lightMode.classList.remove("active");
+    darkMode.classList.add("active");
+    defaultMode.classList.remove("active");
+    changeLogos();
 })
+
+/* Change logos for dark or light mode */
+
+const logoAlpine = document.querySelector(".logoAlpine");
+const logoHtmx = document.querySelector(".logoHtmx");
+
+function changeLogos()  {
+    if (bodyElement.classList.contains("defaultMode") || body.classList.contains("lightMode")) {
+        logoAlpine.setAttribute("src", "./css/img/alpine_long.png");
+        logoHtmx.setAttribute("src", "./css/img/htmx.svg");
+    } else {
+        logoAlpine.setAttribute("src", "./css/img/alpine.dark.svg");
+        logoHtmx.setAttribute("src", "./css/img/htmx.dark.svg");
+    }
+}
